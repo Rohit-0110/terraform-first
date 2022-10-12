e4244e385133f8fdd612ca32a41a0caa6de4b992
Rohit-0110 <vishwakarmarohit750@gmail.com>
2022-10-12 22:52:45 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAS3ET7ZJH74AOLTZJ"
+    secret_key = "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" 
+}
+
+variable "cidr_block" {
+    description = "cidr block and name tags for vpc and subnets"
+    type = list(object({
+        cidr_block = string
+        name = string
+    }))
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = var.cidr_block[0].cidr_block
+    tags = {
+        Name: var.cidr_block[0].name
+    }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_block[1].cidr_block
+    availability_zone = "ap-south-1a"
+    tags = {
+        Name: var.cidr_block[1].name
+    }
+}
+
+data "aws_vpc" "existing_vpc" {
+    default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = "172.31.48.0/20"
+    availability_zone = "ap-south-1a"
+    tags = {
+        Name: "subnet-2-default"
+    }
+}
+
+output "dev-vpc-id" {
+    value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id" {
+    value = aws_subnet.dev-subnet-1.id
+}
